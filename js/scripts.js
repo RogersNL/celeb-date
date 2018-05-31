a0a13f6ebe852758010f955b1110bd692dcb303c
Alex Bunnell and Nick Rogers <nick@>
2018-05-31 16:23:46 -0700

    Finalize JS and HTMLm include pictures for each candidate as well as responses for input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,64 @@
 $(document).ready(function(){
 
-$('#yourinfo').submit(funtion(event){
-  var inputName =
-  var inputGender =
-  var inputAge =
-  var inputDate = 
+$('#yourInfo').submit(function(event){
+  event.preventDefault();
+  var inputName = $('input#user-name').val();
+  var inputGender = $('select#gender').val();
+  var inputAge = parseInt($('select#user-age').val());
+  var inputDate = parseInt($('select#user-date').val());
+
+  var result = inputAge + inputDate
+
+  $(".inputtedName").text(inputName);
+
+  if (inputGender === "Male") {
+    if (result >=6 ) {
+      $("#matchEarhart").show();
+      $("#matchMonroe").hide();
+      $("#matchAaliyah").hide();
+      $("#matchBowie").hide();
+      $("#matchPrince").hide();
+      $("#matchKobain").hide();
+
+    } else if (result >= 4) {
+      $("#matchMonroe").show();
+      $("#matchEarhart").hide();
+      $("#matchAaliyah").hide();
+      $("#matchBowie").hide();
+      $("#matchPrince").hide();
+      $("#matchKobain").hide();
+    } else {
+      $("#matchAaliyah").show();
+      $("#matchMonroe").hide();
+      $("#matchEarhart").hide();
+      $("#matchBowie").hide();
+      $("#matchPrince").hide();
+      $("#matchKobain").hide();
+    }
+  } else {
+     if (result >=6 ) {
+      $("#matchBowie").show();
+      $("#matchMonroe").hide();
+      $("#matchAaliyah").hide();
+      $("#matchEarhart").hide();
+      $("#matchPrince").hide();
+      $("#matchKobain").hide();
+    } else if (result >= 4) {
+      $("#matchPrince").show();
+      $("#matchMonroe").hide();
+      $("#matchAaliyah").hide();
+      $("#matchBowie").hide();
+      $("#matchEarhart").hide();
+      $("#matchKobain").hide();
+    } else {
+      $("#matchKobain").show();
+      $("#matchMonroe").hide();
+      $("#matchAaliyah").hide();
+      $("#matchBowie").hide();
+      $("#matchPrince").hide();
+      $("#matchEarhart").hide();
+    }
+  }
 
 
 });
